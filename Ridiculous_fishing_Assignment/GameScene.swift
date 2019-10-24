eac743f086283b7bddc8afd59adf4da04741df80
Nachiketa <nachiketa0333@gmail.com>
2019-10-24 16:06:24 -0400

    // try to move the background endless

--- Ridiculous_fishing_Assignment/GameScene.swift
+++ Ridiculous_fishing_Assignment/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     
     var water: SKSpriteNode!
     var mud: SKSpriteNode!
+    var ground = SKSpriteNode()
+    
     
     
     var mouseX:CGFloat = 0
@@ -20,11 +22,40 @@ class GameScene: SKScene {
     
     
     override func didMove(to view: SKView) {
+        
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        createGround()
         let background = SKSpriteNode(imageNamed: "background1")
         background.position = CGPoint(x: size.width/2, y: size.height/2)
         addChild(background)
     }
     
+    override func update(_ currentTime: TimeInterval) {
+        moveGround()
+    }
+    
+    func createGround(){
+        for i in 0...3{
+            let ground = SKSpriteNode(imageNamed: "Images")
+            ground.name = "Ground"
+            ground.size = CGSize(width: (self.scene?.size.width)!, height: 250)
+            ground.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            ground.position = CGPoint(x: CGFloat(i) * ground.size.width, y: -(self.frame.size.height / 2))
+            
+            self.addChild(ground)
+        }
+    }
+      
+    func moveGround() {
+        self.enumerateChildNodes(withName: "ground") { (node, error) in
+            node.position.x -= 2
+            
+            if node.position.x < -((self.scene?.size.width)!){
+                node.position.x += (self.scene?.size.width)! * 3
+            }
+        }
+    }
     
     var fish:[SKSpriteNode] = []
     
